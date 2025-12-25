a6b86d865513931ddb7c528bb7023b83cc13f732
bbalbayrak <b.b.albayrak@hotmail.com>
2025-12-25 18:48:07 +0300

    Add Departments, Lectures, and Notes modules with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { UniversitiesModule } from './modules/universities/universities.module';
+import { DepartmentsModule } from './modules/departments/departments.module';
+import { LecturesModule } from './modules/lectures/lectures.module';
+import { NotesModule } from './modules/notes/notes.module';
 
 @Module({
-  imports: [UsersModule, UniversitiesModule],
+  imports: [UsersModule, UniversitiesModule, DepartmentsModule, LecturesModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
